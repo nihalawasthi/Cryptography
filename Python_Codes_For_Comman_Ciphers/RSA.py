e81fe6c35d9ac0f16d72cb365fc1bcb6db27824f
Nihal Awasthi <137594290+nihalawasthi@users.noreply.github.com>
2024-05-23 22:01:39 +0530

    Add files via upload

--- Python_Codes_For_Comman_Ciphers/RSA.py
+++ Python_Codes_For_Comman_Ciphers/RSA.py
@@ -1,128 +1,117 @@
+import random
 import math
-import string
 
-main=string.ascii_lowercase
+# A set will be the collection of prime numbers,
+# where we can select random primes p and q
+prime = set()
 
-# Pseudo code for GCD is
-'''
-function gcd(a, b)
-    if b = 0
-        return a
-    else
-        return gcd(b, a mod b)
-'''
+public_key = None
+private_key = None
+n = None
 
-# Naive method finding the multiplicative inverse of two numbers
-def multiplicative_inverse(a, m):
-    a=a%m; 
-    for x in range(1,m) : 
-        if((a*x)%m==1) : 
-            return x 
-    return 1
+# We will run the function only once to fill the set of
+# prime numbers
+def primefiller():
+	# Method used to fill the primes set is Sieve of
+	# Eratosthenes (a method to collect prime numbers)
+	seive = [True] * 250
+	seive[0] = False
+	seive[1] = False
+	for i in range(2, 250):
+		for j in range(i * 2, 250, i):
+			seive[j] = False
 
-# Fast modular exponentiation function (can be used when
-# something is very large for calculation modular)
-'''
-def get_mod_expo(base,exponent,modulus):
-    result=1
-    while exponent:
-        d=exponent%2
-        exponent=exponent//2
-        if d:
-            result=result*base%modulus
-        base=base*base%modulus
-    return result
-'''
+	# Filling the prime numbers
+	for i in range(len(seive)):
+		if seive[i]:
+			prime.add(i)
 
-# Function to generate a public and private key pair
-def generate_keypair(p, q):
-    n=p*q
-    print("Value of n: ",n)
 
-    # Phi is the Euler's totient of n
-    phi = (p-1)*(q-1)
-    print("Value of phi(n): ", phi)
+# Picking a random prime number and erasing that prime
+# number from list because p!=q
+def pickrandomprime():
+	global prime
+	k = random.randint(0, len(prime) - 1)
+	it = iter(prime)
+	for _ in range(k):
+		next(it)
 
-    # Choose an integer e such that e and phi(n) are co-prime
-    # e = random.randrange(1, phi) for random pick
-    print("Enter e such that is co-prime to ", phi,": ")
-    e=int(input())
+	ret = next(it)
+	prime.remove(ret)
+	return ret
 
-    # Using Euclid's Algorithm to verify that e and phi(n) are co-prime
-    # THe built in function gcd helps with the same
-    g=math.gcd(e,phi)
-    while(g!=1):
-        print("The number you entered is not co-prime")
-        e=int(input())
-        g=math.gcd(e,phi)
+
+def setkeys():
+	global public_key, private_key, n
+	prime1 = pickrandomprime() # First prime number
+	prime2 = pickrandomprime() # Second prime number
+
+	n = prime1 * prime2
+	fi = (prime1 - 1) * (prime2 - 1)
+
+	e = 2
+	while True:
+		if math.gcd(e, fi) == 1:
+			break
+		e += 1
+
+	# d = (k*Φ(n) + 1) / e for some integer k
+	public_key = e
+
+	d = 2
+	while True:
+		if (d * e) % fi == 1:
+			break
+		d += 1
+
+	private_key = d
+
+
+# To encrypt the given number
+def encrypt(message,public_key, n):
+    e = public_key
+    encrypted_text = 1
+    encoded = []
+
+    for letter in message:
+        encrypted_text = 1
+        encrypted_text = pow(ord(letter), e, n)
+        encoded.append(encrypted_text)
         
-    print("Value of exponent(e) entered is: ", e)
-
-    # To generate the private key
-    d = multiplicative_inverse(e, phi)
-    # We can use Extended Euclidean Algorithm because
-    # we know that e and phi are coprimes
+    encoded = ''.join(str(p) for p in encoded)
     
-    # Public key is (e, n) and private key is (d, n)
-    return (e,n),(d,n)
-
-# Function to Encrypt the message
-def encrypt(public_key, to_encrypt):
-    key, n = public_key
-
-    # we can also use fast modular exponentiation here
-    cipher=pow(to_encrypt,key)%n
-    return cipher
+    return encoded
 
 
-# Function to Decrypt the message
-def decrypt(private_key, to_decrypt):
-    key, n = private_key
-
-    # we can also use fast modular exponentiation here
-    decrypted=pow(to_decrypt,key)%n
-    return decrypted
-
-# Main Program
-# primes of 8 bits in length in binary
-p=int(input("Enter prime p: "))
-q=int(input("Enter prime q (!=p): "))
-
-# to make sure that p not equal to q while generating randomly
-while(p==q):
-    p=int(input("Enter prime p: "))
-    q=int(input("Enter prime q (!=p): "))
+# To decrypt the given number
+def decrypt(encoded):
+    global private_key, n
+    d = private_key
+    decrypted_text = ''
     
-print("Prime number p: ",p)
-print("Prime number q: ",q)
+    # Convert the string back to a list of numbers
+    encoded_list = [int(encoded[i:i+4]) for i in range(0, len(encoded), 4)]
 
-print("Generating Public/Private key-pairs!")
-public, private = generate_keypair(p, q)
-print("Your public key is (e,n) ", public)
-print("Your private key is (d,n) ", private)
+    # Decrypting and decoding each number
+    for num in encoded_list:
+        decrypted_text += chr(pow(num, d, n))  # RSA decryption
+    
+    return decrypted_text
 
-message = input("Enter the message: ")
+primefiller()
+setkeys()
+message ='0011011000110100001101110011001000110000'
+coded = encrypt(message,public_key, n)
 
-# converting into lower case and removing spaces
-message=message.replace(" ","")
-message=message.lower()
-arr=[]
-cipher_text=[]
-for i in message:
-    if i in main:
-        arr.append(main.index(i))
-for i in arr:
-    cipher_text.append(encrypt(public,i))
+print("Initial message:")
+print(message)
+print("\n\nThe encoded message(encrypted by public key)\n")
+print(coded)
+print("\n\nThe decoded message(decrypted by public key)\n")
+print(decrypt(coded))
+	
 
-print("Encrypted message (Cipher Text): ",cipher_text)
-
-plain=[]
-for i in cipher_text:
-    plain.append(decrypt(private,i))
-plain_text=''
-for i in plain:
-    plain_text=plain_text+main[i]
-print("Plain text array: ",plain)
-print("Decrypted message (Plain Text): ", plain_text)
+#p = int(input("Enter prime p: "))
+#q = int(input("Enter prime q (!=p): "))
 
 
